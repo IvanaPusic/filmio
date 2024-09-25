4a601a734356dc89e683634c2fbfb38a71f287b6
Ivana <ivana@192.168.100.19>
2024-09-25 13:37:58 +0200

    create pages

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import AppLayout from "./AppLayout";
+import Bookmarked from "./Bookmarked";
+import ErrorPage from "./ErrorPage";
+import Homepage from "./Homepage";
+import Login from "./Login";
+import Movies from "./Movies";
+import Signup from "./Signup";
+import TvSeries from "./TvSeries";
+
+export { AppLayout, Bookmarked, ErrorPage, Homepage, Login, Movies, Signup, TvSeries }
